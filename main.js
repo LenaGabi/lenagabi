573ee3e533d06267ae58d4fee01610aa570be29b
LenaGabi <166019755+LenaGabi@users.noreply.github.com>
2024-09-19 12:06:58 +0000

    pagina js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+const botoes = document.querySelectorA11(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
+for(let j = 0; j < botoes.length; j++){
+    botoes[j].onclick = function(){
+        
+    }
+}
+       
